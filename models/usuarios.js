4e0fc0cff5bbb17f25d3ca6a5e281c4a48b36e2c
Ark <mgouget@gmail.com>
2020-10-15 15:07:20 -0300

    se corrigen errores

--- models/usuarios.js
+++ models/usuarios.js
@@ -1,16 +1,15 @@
 const bd = require("./../utils/bd");
-const dbService = require("../utils/dbServices");
 
 const getAll = () => bd("usuarios").select("*");
 
 const getSingle = (id) =>
     bd("usuarios")
-        .where({ id, habilitado: true })
+        .where({ id })
         .select("id", "nombre", "apellido", "correo");
 
 
 
-const create = (obj) => dbService.create("usuarios", obj);
+const create = (obj) => bd("usuarios").insert(obj);
 const modify = (id, obj) => bd("usuarios").where({ id }).update(obj);
 // Row data
 module.exports = { getAll, getSingle , create, modify };
--- utils/dbServices.js
+++
@@ -1,8 +0,0 @@
-const bd = require("./bd");
-
-const create = (tableName, obj) => db(tableName).insert(obj);
-
-const modify = (tableName, obj, conditions) =>
-    db(tableName).where(conditions).insert(obj);
-
-module.exports = { create, modify }; 
